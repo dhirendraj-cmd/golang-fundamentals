b4286b67bea3349d54de6f7470fe47facc3b27ff
dhirendraj-cmd <djha5051@gmail.com>
2025-10-04 18:35:53 +0530

    variables in go

---
+++ 3_variables/variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var name string = "Holmes is learning golang"
+
+	var x = 32
+
+
+	fmt.Println(name)
+
+	fmt.Println("Age is: ", x)
+
+	// shorthand syntax or idiomatic way to declare varaibles in go
+
+	address:=221
+
+	fmt.Println("address is: ", address,"B")
+
+
+
+}
+
